711c4d1b68416c731af36fa308ba2008319de3d4
Mateo Duke <maduke@udel.edu>
2021-01-20 17:54:55 -0500

    updated app.js

--- js/app.js
+++ js/app.js
@@ -1,21 +1,3 @@
-$.navigation = $('nav > ul.nav');
-$(document).ready(function($){
-
-    // Add class .active to current link
-    $.navigation.find('a').each(function(){
-  
-      var cUrl = String(window.location).split('?')[0];
-  
-      if (cUrl.substr(cUrl.length - 1) == '#') {
-        cUrl = cUrl.slice(0,-1);
-      }
-  
-      if ($($(this))[0].href==cUrl) {
-        $(this).addClass('active');
-  
-        $(this).parents('ul').add(this).each(function(){
-          $(this).parent().addClass('open');
-        });
-      }
-    });
+$(".nav-tabs a").click(function(){
+    $(this).tab("show")
 });
